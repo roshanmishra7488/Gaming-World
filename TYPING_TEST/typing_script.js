38debbfdafa8bb88f95002b8c20490438178a7e7
Your Name <your-email@example.com>
2026-04-17 11:56:31 +0530

    update typing test

--- TYPING_TEST/typing_script.js
+++ TYPING_TEST/typing_script.js
@@ -1,126 +1,172 @@
- let typeContent = document.querySelector(".typeContent p");
-let input = document.getElementById("typeValue");
-let resetBtn = document.querySelector(".bottomPart button");
+let gameOverCount = 0;
 
-let soundBtn = document.querySelector(".sound input");
+    let AdModal = document.getElementById("AdModal");
+    let AdVideo = document.getElementById("AdVideo");
 
-let letterIndex = (mistakes = isTyping = 0);
+    let typeContent = document.querySelector(".typeContent p");
+    let input = document.getElementById("typeValue");
+    let resetBtn = document.querySelector(".bottomPart button");
 
-let time;
-let tLeft = document.querySelector(".tLeft");
-let error = document.querySelector(".error");
-let wpm = document.querySelector(".wpm");
-let cpm = document.querySelector(".cpm");
-let maxTime = 60;
-let timeLeft = maxTime;
+    let timeOverBox = document.getElementById("timeOverBox");
 
-let correctType = new Audio("typing_sound/type.mp3");
-let incorrectType = new Audio("typing_sound/wrong.mp3");
+    let soundBtn = document.querySelector(".sound input");
 
-let isSoundOn = true;
-
-let playSound = () => {
-    correctType.pause();
-    incorrectType.pause();
-}
-
-let loadPara = () => {
-    let randomPara = Math.floor(Math.random() * article.length);
-    typeContent.innerHTML = "";
-    article[randomPara].split('').forEach(element => {
-        let realData = `<span>${element}</span>`;
-        typeContent.innerHTML += realData;
+    document.querySelector(".box").addEventListener('click', () => {
+        if (!input.disabled) {
+            input.focus();
+        }
     });
 
-    typeContent.querySelectorAll('span')[0].classList.add('active');
-    document.addEventListener('click', () => {
-        input.focus();
-    })
-}
+    let letterIndex = 0;
+    let mistakes = 0;
+    let isTyping = false;
 
-loadPara();
+    let time;
+    let tLeft = document.querySelector(".tLeft");
+    let error = document.querySelector(".error");
+    let wpm = document.querySelector(".wpm");
+    let cpm = document.querySelector(".cpm");
+    let maxTime = 60;
+    let timeLeft = maxTime;
 
-soundBtn.addEventListener('change', () => {
-    isSoundOn = soundBtn.checked;
-});
+    let correctType = new Audio("typing_sound/type.mp3");
+    let incorrectType = new Audio("typing_sound/wrong.mp3");
 
-input.addEventListener('input', (e) => {
+    let isSoundOn = true;
+
+    let playSound = () => {
+        correctType.pause();
+        incorrectType.pause();
+
+        correctType.currentTime = 0;
+        incorrectType.currentTime = 0;
+    }
+
+    let loadPara = () => {
+        let randomPara = Math.floor(Math.random() * article.length);
+        typeContent.innerHTML = "";
+        article[randomPara].split('').forEach(element => {
+            let realData = `<span>${element}</span>`;
+            typeContent.innerHTML += realData;
+        });
+
+        typeContent.querySelectorAll('span')[0].classList.add('active');
+
+
+    }
+
+    loadPara();
+
+    soundBtn.addEventListener('change', () => {
+        isSoundOn = soundBtn.checked;
+    });
+
+   input.addEventListener('input', (e) => {
     let char = typeContent.querySelectorAll("span");
-    let inputValue = e.target.value.split('')[letterIndex];
+    if (input.disabled) return;
+
+    let inputValue = e.target.value;
 
     if (!isTyping) {
         time = setInterval(timeSetup, 1000);
         isTyping = true;
     }
 
-    if(letterIndex < char.length){
-        if(inputValue == null){
-            if(letterIndex > 0){
-                letterIndex--;
-                if(char[letterIndex].classList.contains('incorrect')){
-                    mistakes--;
-                }
-                char[letterIndex].classList.remove('correct', 'incorrect');
-            }
-        } else {
-            if (char[letterIndex].innerText == inputValue) {
+    //  BACKSPACE HANDLE
+    if (inputValue.length < letterIndex) {
+        letterIndex--;
+        if (char[letterIndex].classList.contains('incorrect')) {
+            mistakes--;
+        }
+        char[letterIndex].classList.remove('correct', 'incorrect');
+    } 
+    //  NORMAL TYPING
+    else {
+        let currentChar = inputValue[letterIndex];
+
+        if (letterIndex < char.length) {
+            if (char[letterIndex].innerText == currentChar) {
                 char[letterIndex].classList.add('correct');
                 playSound();
-                if(isSoundOn){
-                correctType.play();
-                }
+                if (isSoundOn) correctType.play();
             } else {
                 char[letterIndex].classList.add('incorrect');
                 mistakes++;
                 playSound();
-                if(isSoundOn){
-                incorrectType.play();
-                }
+                if (isSoundOn) incorrectType.play();
             }
             letterIndex++;
         }
-        char.forEach(element => {
-            element.classList.remove('active');
-        })
-        if(char[letterIndex]){
-            char[letterIndex].classList.add('active');
-        } else {
-            char[char.length - 1].classList.add('active');
-        }
-        error.innerText = `Mistakes: ${mistakes}`;
-
-        cpm.innerText = `CPM: ${letterIndex - mistakes}`;
-    } else {
-        clearInterval(time);
-        input.value = "";
     }
+
+    // ACTIVE CURSOR
+    char.forEach(el => el.classList.remove('active'));
+    if (char[letterIndex]) {
+        char[letterIndex].classList.add('active');
+    }
+
+    error.innerText = `Mistakes: ${mistakes}`;
+    cpm.innerText = `CPM: ${letterIndex - mistakes}`;
 });
 
-let timeSetup = () => {
-    if(timeLeft > 0){
-        timeLeft--;
-        tLeft.innerText = `Time-Left: ${timeLeft}S`;
+    let timeSetup = () => {
+        if (timeLeft > 0) {
+            timeLeft--;
+            tLeft.innerText = `Time-Left: ${timeLeft}S`;
 
-        let timeSpent = maxTime - timeLeft;
-        if(timeSpent === 0){
-            timeSpent = 1;
+            let timeSpent = maxTime - timeLeft || 1;
+
+            let wpmTab = Math.round((letterIndex - mistakes) / 5 / timeSpent * 60);
+            wpm.innerText = `WPM: ${wpmTab}`;
+        } else {
+            clearInterval(time);
+            time = null;
+            input.value = "";
+            input.blur();
+
+            input.disabled = true;
+
+            setTimeout(() => {
+            gameOverCount++;
+
+            if (gameOverCount % 3 === 0) {
+                timeOverBox.style.display = "none";
+                AdModal.style.display = "flex";
+                AdVideo.play();
+            } else {
+
+                timeOverBox.style.display = "block";
+            }
+            }, 100);
         }
+    };
 
-        let wpmTab = Math.round((letterIndex - mistakes) / 5 / timeSpent * 60);
-        wpm.innerText = `WPM: ${wpmTab}`;
-    } else {
+    resetBtn.addEventListener('click', () => {
+        loadPara();
         clearInterval(time);
-        input.value = "";
-    }
-};
+        time = null;
 
-resetBtn.addEventListener('click', () => {
-    loadPara();
-    clearInterval(time);
-     wpm.innerText = `WPM: 0`;
-     cpm.innerText = `CPM: 0`;
-     timeLeft = maxTime;
-     tLeft.innerText = `Time-Left: ${maxTime}S`;
-     input.value = "";
-     letterIndex = mistakes = isTyping = 0;
-})
+        timeLeft = maxTime;
+        letterIndex = 0;
+        mistakes = 0;
+        isTyping = false;
+
+        wpm.innerText = `WPM: 0`;
+        cpm.innerText = `CPM: 0`;
+        error.innerText = `Mistakes: 0`;
+        tLeft.innerText = `Time-Left: ${maxTime}S`;
+
+        input.value = "";
+        input.disabled = false;
+        input.focus();
+
+        timeOverBox.style.display = "none";
+        AdModal.style.display = "none";
+    })
+
+    AdVideo.addEventListener("ended", () => {
+        AdModal.style.display = "none";
+        timeOverBox.style.display = "block";
+
+        AdVideo.currentTime = 0;
+    });
